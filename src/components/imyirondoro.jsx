6d9254fe32def0a77f6acd7809d71708ec8caf8e
AbayoHJovin <abayohirwajovin@gmail.com>
2024-07-13 17:25:02 +0200

    Added user

--- src/components/imyirondoro.jsx
+++ src/components/imyirondoro.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 export default function Imyirondoro() {
+  const [isChristian, setIsChristian] = useState("");
   const navigate = useNavigate();
-  function handleSubmit(e){
-    e.preventDefault()
-    navigate("/dashboard")
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (isChristian === "abakristu") {
+      navigate("/userdashboard");
+    } else {
+      navigate("/dashboard");
+    }
   }
+
   return (
     <div>
       <nav className="flex bg-green-800 w-full z-50 p-4 items-center gap-3 text-white cursor-pointer sticky top-0">
@@ -24,7 +31,10 @@ export default function Imyirondoro() {
             className="rounded-lg w-[300px] h-[300px]"
           />
         </div>
-        <form onSubmit={handleSubmit} className="max-w-md w-full bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-md w-full bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
+        >
           <h1 className="text-xl font-bold mb-4 text-center text-green-700">
             Andika amazina yawe
           </h1>
@@ -52,7 +62,11 @@ export default function Imyirondoro() {
           <h1 className="text-xl font-bold mb-4 text-center text-green-700">
             Andika Hitamo icyo ushinzwe
           </h1>
-          <select className="w-full mb-4 p-2 border-2 border-green-800 rounded-md">
+          <select
+            value={isChristian}
+            onChange={(e) => setIsChristian(e.target.value)}
+            className="w-full mb-4 p-2 border-2 border-green-800 rounded-md"
+          >
             <option value="pasiteri">Pasiteri</option>
             <option value="ishuri">Ishuri</option>
             <option value="umutungo">Umutungo</option>
